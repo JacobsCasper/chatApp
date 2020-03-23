3cd41bfeec24498f74651f50f1fe332a8a4f5ea7
Casper Jacobs <casper.jacobs@student.pxl.be>
2020-03-23 15:03:08 +0100

    Add project files.

---
+++ chatApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using chatApp.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace chatApp.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public DbSet<Message> Messages { get; set; }
+        public DbSet<AppUser> AppUsers { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Message>()
+                .HasOne<AppUser>(a => a.Sender)
+                .WithMany(m => m.Messages)
+                .HasForeignKey(m => m.UserId);
+            base.OnModelCreating(builder);
+        }
+    }
+}
---
+++ chatApp/Hubs/ChatHub.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using chatApp.Models;
+using Microsoft.AspNetCore.SignalR;
+
+namespace chatApp.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(Message message) =>
+            await Clients.All.SendAsync("recieveMessage", message);
+    }
+}
